5344628e76a60fbaabca47085c7fa117bd4a99fd
M@thieu Ch@mb@ud <mathchambaud2000@gmail.com>
2023-11-14 13:32:36 +0100

    update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@
 
 import { PrismaClient } from '@prisma/client';
 
-export const prisma: PrismaClient = global.prisma ?? new PrismaClient({
+export const prisma: PrismaClient = new PrismaClient({
     log: process.env.NODE_ENV === "development"
     ? ['query', 'error', 'warn']
     : ['error'] 
